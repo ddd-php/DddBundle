f8f0ea73f9977007b1ae0d953a267cca3a47112e
Jean-François Simon <contact@jfsimon.fr>
2013-03-14 07:41:59 +0100

    Integrated slug component

---
+++ DddBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Ddd\Bundle\DddBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Bundle class for Ddd components integration.
+ *
+ * @author Jean-François Simon <contact@jfsimon.fr>
+ */
+class DddBundle extends Bundle
+{
+}
---
+++ DependencyInjection/DddExtension.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Ddd\Bundle\DddBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+
+/**
+ * Ddd DIC extension.
+ *
+ * @author Jean-François Simon <contact@jfsimon.fr>
+ */
+class DddExtension extends Extension
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config/services'));
+        $loader->load('slug.xml');
+    }
+}
